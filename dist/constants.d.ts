6da09337b727d33989733aadd59389784f20be5c
tychezone <protocoltyche@gmail.com>
2022-12-11 01:19:12 +0500

    first commit

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x1279aF64A33138792395B16832F9C0fa6F068FCa";
+export declare const INIT_CODE_HASH = "0x2be903b0269b1a1dc2aab081cb7198ec282d010df1d848f2e8d54ee4ceda4eba";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x1279aF64A33138792395B16832F9C0fa6F068FCa' // Tyche Factory on TestNet of EVMOS
+
+export const INIT_CODE_HASH = '0x2be903b0269b1a1dc2aab081cb7198ec282d010df1d848f2e8d54ee4ceda4eba'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
