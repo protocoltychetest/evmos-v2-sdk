7b701c7bbdf8eed32c670039e92fb1da6b19548d
tychezone <protocoltyche@gmail.com>
2023-04-23 23:33:27 +0200

    add zksync testnet

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -2,7 +2,7 @@ import JSBI from 'jsbi';
 export declare const FACTORY_ADDRESS: {
     [key: number]: string;
 };
-export declare const INIT_CODE_HASH = "0x2be903b0269b1a1dc2aab081cb7198ec282d010df1d848f2e8d54ee4ceda4eba";
+export declare const INIT_CODE_HASH = "0xd1c22c9265a6b299435d450062fe36d684a63bd3c86362640d444d20b3f90701";
 export declare const MINIMUM_LIQUIDITY: JSBI;
 export declare const ZERO: JSBI;
 export declare const ONE: JSBI;
