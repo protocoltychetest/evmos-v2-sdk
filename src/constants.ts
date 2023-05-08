9b057fbee0aa6e2796ba9a4e46619c338e530489
tychezone <protocoltyche@gmail.com>
2023-05-08 02:56:33 +0200

    init code for evmos, zk init code most likely not being used

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const FACTORY_ADDRESS: {[key: number]: string} = {
     [ChainId.ZKSYNC_TESTNET]: '0x59E5Dd8C2252250d643D010ba438327e7c430FA3',
 } // Tyche Factory on TestNet of EVMOS
 
-export const INIT_CODE_HASH = '0xd1c22c9265a6b299435d450062fe36d684a63bd3c86362640d444d20b3f90701'
+export const INIT_CODE_HASH = '0x2be903b0269b1a1dc2aab081cb7198ec282d010df1d848f2e8d54ee4ceda4eba'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
